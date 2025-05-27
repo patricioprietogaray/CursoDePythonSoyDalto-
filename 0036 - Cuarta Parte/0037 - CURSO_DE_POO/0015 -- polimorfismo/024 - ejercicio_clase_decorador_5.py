1f18b91fbf2c0bfeeff210c03225f9af1ced255e
Patricio Prieto Garay <93171508+patricioprietogaray@users.noreply.github.com>
2025-05-27 16:47:52 -0300

    Update 024 - ejercicio_clase_decorador_5.py

--- 0036 - Cuarta Parte/0037 - CURSO_DE_POO/0015 -- polimorfismo/024 - ejercicio_clase_decorador_5.py
+++ 0036 - Cuarta Parte/0037 - CURSO_DE_POO/0015 -- polimorfismo/024 - ejercicio_clase_decorador_5.py
@@ -8,6 +8,16 @@
 # específico para cada instancia del objeto 
 # si el método es de instancia, 
 # o para la clase si el método es de clase.
+
+def limitar_llamadas(funcion):
+    def wrapper(self, *args, **kwargs):
+        if self.llamar > self.max_llamadas:
+            return 'Maximo de llamadas permitido'
+        self.llamar += 1
+        return funcion(self, *args, **kwargs)
+    return wrapper
+
+
 # 
 # Crea una clase ServicioWeb.
 # Dentro de ServicioWeb, crea un método de 
@@ -15,9 +25,39 @@
 # que simule hacer una petición. 
 # Aplica el decorador @limitar_llamadas(3) a este método.
 # 
+class ServicioWeb:
+    def __init__(self, url, llamar=1, max_llamadas=2):
+        self.url = url
+        self.llamar = llamar
+        self.max_llamadas = max_llamadas
+        
+    def realizar_peticion(self):
+        return f'realizando la peticion a {self.url}'
+    
+    @classmethod
+    def crear_servicio_tipo(self, tipo_servicio):
+        if tipo_servicio == 'premium':
+            return 'El servicio es premium'
+        else:
+            return 'El servicio es básico'
+        
+
 # Crea un @classmethod en ServicioWeb llamado 
 # crear_servicio_tipo(tipo_servicio). 
 # Este método actuará como una fábrica simple. 
+
+class ServicioWebPremium(ServicioWeb):
+    pass
+
+class ServicioWebBasico(ServicioWeb):
+    # servicio basico de dos llamados
+    def __init__(self, url, llamar=1, max_llamadas=2):
+        super().__init__(url, llamar, max_llamadas)
+        print(limitar_llamadas(max_llamadas))
+    
+
+
+
 # Si tipo_servicio es "premium", devuelve una 
 # instancia de ServicioWebPremium 
 # (que hereda de ServicioWeb). 
@@ -39,3 +79,10 @@
 # No dudes en preguntar si tienes alguna duda con 
 # alguno de ellos. ¡Mucha suerte!
 
+servicioWEB = ServicioWeb('www.abc.com')
+print(servicioWEB.realizar_peticion())
+basico = ServicioWebBasico('www.abc.com')
+print(basico.crear_servicio_tipo('basico'))
+print(basico.realizar_peticion())
+print(basico.realizar_peticion())
+print(basico.realizar_peticion())
